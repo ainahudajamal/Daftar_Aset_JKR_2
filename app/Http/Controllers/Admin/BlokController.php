c4676db3ae9c8b8e28fae61ee872d9ad166d7a79
anisjow <anisjow@gmail.com>
2026-06-07 11:40:22 +0800

    update blok controller

--- app/Http/Controllers/Admin/BlokController.php
+++ app/Http/Controllers/Admin/BlokController.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Models\KodBlok;
 use Illuminate\Http\Request;
 use App\Models\AuditLog;
+use Mpdf\Mpdf;
+
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
+
+
 
 class BlokController extends Controller
 {
@@ -38,9 +44,9 @@ class BlokController extends Controller
     }
 
     public function create()
-    {
-        return view('admin.blok.create');
-    }
+{
+    return view('admin.blok.create');
+}
 
     public function store(Request $request)
     {
@@ -116,4 +122,65 @@ class BlokController extends Controller
         return redirect()->route('admin.blok.index')
             ->with('success', 'Blok berjaya dipadam.');
     }
+    public function exportPdf()
+{
+    $premis = [
+        'nama'   => 'PARLIMEN MALAYSIA',
+        'no_dpa' => '11011 01MYS.14004 4.BD0001',
+    ];
+
+    $bloks = [
+        ['bil' => 1, 'nama' => 'DEWAN PERSIDANGAN', 'fungsi_binaan' => 'DEWAN',   'luas_tapak' => '4,509.26', 'kod_myspata' => 'F'],
+        ['bil' => 2, 'nama' => 'BLOK PEJABAT',      'fungsi_binaan' => 'PEJABAT', 'luas_tapak' => '2,100.00', 'kod_myspata' => 'A'],
+        ['bil' => 3, 'nama' => 'BLOK STOR',          'fungsi_binaan' => 'STOR',    'luas_tapak' => '850.00',   'kod_myspata' => 'B'],
+    ];
+
+    $binaanLuars = [
+        ['bil' => 1, 'nama' => 'PAGAR KAWASAN', 'jenis' => 'PAGAR', 'luas_tapak' => '320.00', 'kod_myspata' => 'PG01'],
+    ];
+
+    $catatan_blok   = '';
+    $catatan_binaan = '';
+
+$mpdf = new Mpdf([
+
+    'margin_top'    => 10,
+    'margin_bottom' => 10,
+    'margin_left'   => 10,
+    'margin_right'  => 10,
+
+    'format'        => 'A4',
+
+    'default_font' => 'fredoka',
+
+    'fontDir' => array_merge(
+        (new \Mpdf\Config\ConfigVariables())->getDefaults()['fontDir'],
+        [
+            storage_path('fonts')
+        ]
+    ),
+
+    'fontdata' => array_merge(
+        (new \Mpdf\Config\FontVariables())->getDefaults()['fontdata'],
+        [
+            'fredoka' => [
+                'R' => 'Fredoka-Regular.ttf',
+            ]
+        ]
+    ),
+
+]);
+
+
+
+    $html = view('admin.blok.pdf', compact(
+        'premis', 'bloks', 'binaanLuars', 'catatan_blok', 'catatan_binaan'
+    ))->render();
+
+    $mpdf->WriteHTML($html);
+
+    return response($mpdf->Output('DA4_Borang_Blok.pdf', 'S'), 200)
+        ->header('Content-Type', 'application/pdf')
+        ->header('Content-Disposition', 'inline; filename="DA4_Borang_Blok.pdf"');
+}
 }
