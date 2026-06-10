f795dc81417ce581151c2aa4a7d21a67baaf09bd
anisjow <anisjow@gmail.com>
2026-06-10 09:28:18 +0800

    Add BinaanLuar model for DA4

---
+++ app/Models/BinaanLuar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BinaanLuar extends Model
+{
+    use HasFactory;
+
+    protected $table = 'binaan_luar';
+
+    protected $fillable = [
+        'premis_id',
+        'bil',
+        'nama_binaan_luar',
+        'jenis_binaan_luar',
+        'luas_tapak',
+        'kod_binaan_luar_myspata',
+        'catatan',
+    ];
+
+    public function premis()
+    {
+        return $this->belongsTo(Premis::class);
+    }
+}
